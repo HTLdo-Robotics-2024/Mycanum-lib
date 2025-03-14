de2ecf78ed82e5c564666bb64ccf0725bdeb2027
ArisBaur <bauraris@gmail.com>
2025-03-14 02:58:47 -0600

    initial commit

---
+++ include/mycanum.hpp
@@ -0,0 +1,13 @@
+#ifndef MYCANUM_H
+
+#define MYCANUM_H
+
+void forward(int time);
+void backward(int time);
+void left(int time);
+void right(int time);
+
+void turn_left(int time);
+void turn_right(int time);
+
+#endif // MYCANUM_H
