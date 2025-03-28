c03eda475f191b7e3f181cfdec03d20ccee487e9
Aris Baur <114926537+ArisBaur@users.noreply.github.com>
2025-03-28 16:40:35 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <kipr/wombat.h>
 #include "mycanum.hpp"
 
-#define pi 3.1415926535897932384626433832795028841971693993751
-
 int main()
 {
     printf("Hello class, today we will figure out how to rotate the wombat by a certain amount of degrees\n");
